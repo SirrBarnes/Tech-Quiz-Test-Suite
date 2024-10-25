c37e2d9f0cb294f2a0f90a27e79c0639f2e74a5e
SirrBarnes <torressergio1357@gmail.com>
2024-10-24 22:18:28 -0400

    made changes to all of cypress folder

---
+++ cypress/e2e/quiz.cy.js
@@ -0,0 +1,9 @@
+import { beforeEach } from "vitest"
+
+// describe (('Tech Quiz Test Suite', () => {
+//     beforeEach(() => {
+//         cy.visit('http://localhost:3000/');
+//     });
+
+    
+// }))
